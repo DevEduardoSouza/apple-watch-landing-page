fb2a606ab7ed77479bbebedbffefc4952305f7a5
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-11-21 18:52:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Technology
+ Projeto Technology
---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector(".menuMobile").addEventListener("click", function () {
+    if(document.querySelector(".menu nav ul").style.display == 'flex'){
+        document.querySelector(".menu nav ul").style.display = 'none';
+        
+    } else {
+        document.querySelector(".menu nav ul").style.display = 'flex';
+        // document.querySelector("header").style.backgroundColor = '#2f2f2f';
+    }
+});
